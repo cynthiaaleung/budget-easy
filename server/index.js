692be5991838b149f83bb3a01ad06209d452469f
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-03-08 16:24:21 -0800

    move db config info to .env file

--- server/index.js
+++ server/index.js
@@ -14,3 +14,4 @@ app.use("/auth", require("./routes/jwtAuth"));
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
+
--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -6,7 +6,7 @@ const jwtGenerator = (id) => {
     user: id
   };
 
-  return jwt.sign(payload, process.env.jwtSecret, {expiresIn: 60 * 60}); // in seconds, here it is 1hr
+  return jwt.sign(payload, process.env.JWTSECRET, {expiresIn: 60 * 60}); // in seconds, here it is 1hr
 };
 
 module.exports = jwtGenerator;
