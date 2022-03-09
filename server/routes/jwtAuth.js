308205befff4eb3556e88b905b0d9fa5abbf1191
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-03-08 16:47:33 -0800

    add login route

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -40,4 +40,39 @@ router.post("/register", async(req, res) => {
   }
 });
 
+
+// Login
+
+router.post("/login", async(req, res) => {
+  try {
+    // destructure req.body
+    const { email, password } = req.body;
+
+    // check if user exists (if user exists then throw error)
+    const user = await pool.query("SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("Invalid credentials");
+    }
+
+    // check if incoming password is same as database password
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
+
+    if (!validPassword) {
+      return res.status(401).json("Invalid credentials");
+    }
+
+    // give them jwt token
+    const token = jwtGenerator(user.rows[0].id);
+
+    res.json({ token });
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
