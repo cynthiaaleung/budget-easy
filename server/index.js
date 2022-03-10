6da8f263aa8e94fa724f7304b08672f8c4df9e06
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-03-10 14:33:29 -0800

    add dashboard route

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ app.use(cors()); // enable backend to interact with frontend
 // Register and login routes
 app.use("/auth", require("./routes/jwtAuth"));
 
+// Dashboard route
+app.use("/dashboard", require("./routes/dashboard"));
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
