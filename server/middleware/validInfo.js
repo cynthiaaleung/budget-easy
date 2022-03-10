ddaedde495aa74c3b0c0299199e9de5a037b07dd
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-03-10 14:14:33 -0800

    create middleware to validate register and login credentials

---
+++ server/middleware/validInfo.js
@@ -0,0 +1,27 @@
+module.exports = (req, res, next) => {
+
+  const { email, name, password } = req.body;
+
+  const validEmail = (userEmail) => {
+    // returns boolean value whether or not email pattern exists in string
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
+  };
+
+  if (req.path === "/register") {
+    // name, email, password inputs are mandatory
+    if (![email, name, password].every(Boolean)) {
+      return res.status(401).json("Missing credentials");
+    } else if (!validEmail(email)) {
+      return res.status(401).json("Invalid email");
+    }
+  } else if (req.path === "/login") {
+    // email and password inputs are mandatory
+    if (![email, password].every(Boolean)) {
+      return res.status(401).json("Missing credentials");
+    } else if (!validEmail(email)) {
+      return res.status(401).json("Invalid email");
+    }
+  }
+  // continue on with the route
+  next();
+};
