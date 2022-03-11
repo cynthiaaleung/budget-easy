3b475b96a90a857dcb1ca23bdb13be695ba5a602
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-03-11 13:45:20 -0800

    move schema to db folder

--- server/schema/01_users.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS users CASCADE;
-
-CREATE TABLE users (
-  id uuid PRIMARY KEY DEFAULT
-  uuid_generate_v4(),
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
