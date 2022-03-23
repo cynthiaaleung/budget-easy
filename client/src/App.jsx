5156d8495338d553060d322f47a3efdfa598ce3c
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-03-23 14:23:56 -0700

    implement routes for Login, Restaurant, Dashboard components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,34 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 import './App.css';
 
 function App() {
   return (
     <Fragment>
-      
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route 
+              exact 
+              path="/login" 
+              element={<Login />}
+            />
+            <Route 
+              exact 
+              path="/register" 
+              element={<Register />}
+            />
+            <Route 
+              exact 
+              path="/dashboard" 
+              element={<Dashboard />}
+            />
+          </Routes>
+        </div>
+      </Router>
     </Fragment>
   );
 }
