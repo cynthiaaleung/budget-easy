5ee6e6bf46153ee8c3ea0259fef881fa16e31b77
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-03-28 13:18:12 -0700

    create login form and button

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,8 +3,20 @@ import { Fragment } from "react"
 const Login = ({ setAuth }) => {
   return (
     <Fragment>
-      <h1>Login</h1>
-      <button onClick={() => setAuth(true)}>Log In</button>
+      <h1 className="text-center my-5">Login</h1>
+      <form>
+        <input 
+          type="email" 
+          name="email" 
+          placeholder="email" 
+          className="form-control my-3"/>
+        <input 
+          type="password" 
+          name="password" 
+          placeholder="password" 
+          className="form-control my-3"/>
+        <button className="btn btn-success btn-block">Submit</button>
+      </form>
     </Fragment>
   )
 }
