94b2040ae5013f38ee0c5612afd541e64061d40e
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-04-12 11:52:58 -0700

    implement logic for get request for user name and display on page

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,32 @@
-import { Fragment } from "react"
+import { Fragment, useState, useEffect } from "react"
 
 const Dashboard = ({ setAuth }) => {
+
+  const [name, setName] = useState("");
+
+  const getName = async() => {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard/", {
+        method: "GET",
+        headers: { token: localStorage.token }
+      })
+
+      const parseRes = await response.json();
+
+      setName(parseRes.name);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getName();
+  })
+
   return (
     <Fragment>
       <h1>Dashboard</h1>
+      <h2>Hello {name} !</h2>
     </Fragment>
   )
 }
